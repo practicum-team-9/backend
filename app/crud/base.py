2a28fbea606992a23af53860235f95b4286a0a74
Ilya Potashev <potaschev.ilya@yandex.ru>
2025-09-27 12:33:49 +0300

    Update base.py

---
+++ app/crud/base.py
@@ -0,0 +1,44 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+class CRUDBase:
+
+    def __init__(self, model):
+        self.model = model
+
+    async def get_by_id(
+            self,
+            obj_id: int,
+            session: AsyncSession
+    ):
+        db_obj = await session.execute(
+            select(self.model).where(
+                self.model.id == obj_id
+            )
+        )
+        return db_obj.scalars().first()
+
+    async def get_multi(self, session: AsyncSession):
+        db_objs = await session.execute(select(self.model))
+        return db_objs.scalars().all()
+
+    async def create(
+            self,
+            obj_in,
+            session: AsyncSession,
+    ):
+        obj_in_data = obj_in.model_dump()
+        db_obj = self.model(**obj_in_data)
+        session.add(db_obj)
+        await session.commit()
+        await session.refresh(db_obj)
+        return db_obj
+
+    async def remove(
+            self,
+            db_obj,
+            session: AsyncSession
+    ):
+        await session.delete(db_obj)
+        await session.commit()
