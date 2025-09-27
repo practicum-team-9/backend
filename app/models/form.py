809f5e1af79cb011ce094cbf70322a2b3c09587e
Илья Поташев <potaschev.ilya@yandex.ru>
2025-09-27 21:05:29 +0300

    added_form_model

---
+++ app/models/form.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.core.db import Base
+
+
+class Form(Base):
+    """Модель формы."""
+
+    text: Mapped[str] = mapped_column(nullable=False)
+    url: Mapped[str] = mapped_column(nullable=False)
+
+    def __repr__(self):
+        return f"Объект формы. Текст: {self.text[:20]}"
