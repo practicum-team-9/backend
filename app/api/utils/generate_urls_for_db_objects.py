e27da9d5d86813b9a69183dde4917ab809bc2581
Илья Поташев <potaschev.ilya@yandex.ru>
2025-11-08 10:40:39 +0300

    self_url_logic_changed

--- app/api/utils/generate_urls_for_db_objects.py
+++ app/api/utils/generate_urls_for_db_objects.py
@@ -6,4 +6,4 @@ async def generate_tg_url(identifier: str) -> str:
 
 
 async def generate_self_url(id: int) -> str:
-    return settings.self_url + f"/v1/forms/get-form/{id}"
+    return settings.self_url + f"{id}"
